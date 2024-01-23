a16da11f70b2f345a63c9c90e6ed196275f7e74e
P0tamato <2669552578@qq.com>
2024-01-23 20:40:46 +0800

    1

--- Assets/Scripts/Audio/AudioData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[System.Serializable]
-public class AudioData
-{
-    public float volume;
-    public AudioClip clip;
-}
