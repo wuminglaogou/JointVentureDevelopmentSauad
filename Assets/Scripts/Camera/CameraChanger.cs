d7a11b975e2b38e44dba539b95b2d3c83d049258
linyuu123 <2692215468@qq.com>
2024-01-24 22:31:14 +0800

    摄像头切换

---
+++ Assets/Scripts/Camera/CameraChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChanger : MonoBehaviour
+{
+    public GameObject camera0;
+    public GameObject camera1;
+    public GameObject camerachanger0;
+    public GameObject camerachanger1;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        camera0.SetActive(false);
+        camera1.SetActive(true);
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        camerachanger0.SetActive(false);
+        camerachanger1.SetActive(true);
+    }
+}
